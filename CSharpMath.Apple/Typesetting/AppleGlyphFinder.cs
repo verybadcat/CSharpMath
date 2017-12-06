9aacc919fe038c8552c4947b81393e0a48e0a5f5
William Jockusch <jockusch@gmail.com>
2017-12-06 07:57:02 -0600

    Hopefully a correct implementation of AppleGlyphFinder?

--- CSharpMath.Apple/Typesetting/AppleGlyphFinder.cs
+++ CSharpMath.Apple/Typesetting/AppleGlyphFinder.cs
@@ -3,6 +3,11 @@ using CoreText;
 using CSharpMath.FrontEnd;
 using Foundation;
 using TGlyph = System.UInt16;
+using System.Globalization;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace CSharpMath.Apple
 {
   public class AppleGlyphFinder: IGlyphFinder<TGlyph>
@@ -14,14 +19,34 @@ namespace CSharpMath.Apple
     }
     public ushort FindGlyphForCharacterAtIndex(int index, string str)
     {
-      throw new NotImplementedException();
-      //var nsString = new NSString(str);
-      //var range = nsString.Ra
+      var unicodeIndexes = StringInfo.ParseCombiningCharacters(str);
+      int start = 0;
+      int end = str.Length;
+      foreach (var unicodeIndex in unicodeIndexes) {
+        if (unicodeIndex <= index) {
+          start = unicodeIndex;
+        } else {
+          end = unicodeIndex;
+          break;
+        }
+      }
+      var encoding = new UnicodeEncoding();
+      var substring = str.Substring(start, end - start);
+      var encodeSubstring = encoding.GetBytes(substring);
+      return encodeSubstring[0];
     }
 
-    public ushort[] FindGlyphs(string str)
+    private IEnumerable<ushort> FindGlyphsInternal(string str)
     {
-      throw new NotImplementedException();
+      // not completely sure this is correct. Need an actual
+      // example of a composed character sequence coming from LaTeX.
+      var unicodeIndexes = StringInfo.ParseCombiningCharacters(str);
+      foreach (var index in unicodeIndexes) {
+        yield return FindGlyphForCharacterAtIndex(index, str);
+      }
     }
+
+    public ushort[] FindGlyphs(string str) 
+      => FindGlyphsInternal(str).ToArray();
   }
 }
