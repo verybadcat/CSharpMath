60fa3f9f62631877c99c536aa9a3d8ae8d5c0dec
William Jockusch <jockusch@gmail.com>
2017-12-09 19:18:37 -0600

    editorconfig works here. Let's see if we can get away without the .src folder.

--- CSharpMath.Apple/Drawing/AppleGraphicsContext.cs
+++ CSharpMath.Apple/Drawing/AppleGraphicsContext.cs
@@ -6,39 +6,39 @@ using System.Text;
 
 namespace CSharpMath.Apple.Drawing
 {
-    public class AppleGraphicsContext : IGraphicsContext
+  public class AppleGraphicsContext : IGraphicsContext
+  {
+    public CGContext CgContext { get; set; }
+    public void DrawLine(float x1, float y1, float x2, float y2)
     {
-        public CGContext CgContext { get; set; }
-        public void DrawLine(float x1, float y1, float x2, float y2)
-        {
-            CgContext.MoveTo(x1, y1);
-            CgContext.AddLineToPoint(x2, y2);
-            CgContext.DrawPath(CGPathDrawingMode.Stroke);
-        }
-
-        public void DrawTextAtPoint(string text, PointF point, float maxWidth = float.NaN)
-        {
-            
-        }
-
-        public void RestoreState()
-        {
-            CgContext.RestoreState();
-        }
-
-        public void SaveState()
-        {
-            CgContext.SaveState();
-        }
-
-        public void SetTextPosition(PointF position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Translate(PointF dxy)
-        {
-            CgContext.TranslateCTM(dxy.X, dxy.Y);
-        }
+      CgContext.MoveTo(x1, y1);
+      CgContext.AddLineToPoint(x2, y2);
+      CgContext.DrawPath(CGPathDrawingMode.Stroke);
     }
+
+    public void DrawTextAtPoint(string text, PointF point, float maxWidth = float.NaN)
+    {
+
+    }
+
+    public void RestoreState()
+    {
+      CgContext.RestoreState();
+    }
+
+    public void SaveState()
+    {
+      CgContext.SaveState();
+    }
+
+    public void SetTextPosition(PointF position)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Translate(PointF dxy)
+    {
+      CgContext.TranslateCTM(dxy.X, dxy.Y);
+    }
+  }
 }
