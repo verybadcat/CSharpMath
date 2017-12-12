31596f53f5135a6cbc6099e86e19a6caca9d1924
William Jockusch <jockusch@gmail.com>
2017-12-11 19:44:03 -0600

    Recreated iOS project. Created simple ViewController.

--- CSharpMath.Ios/IosMath/IosMathLabels.cs
+++ CSharpMath.Ios/IosMath/IosMathLabels.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using CSharpMath.Apple;
-using Foundation;
-using UIKit;
+﻿using CSharpMath.Apple;
 
-namespace CSharpMath.Ios.IosMath {
-  static class IosMathLabels {
-    public static AppleLatexView LatexView(string latex) {
+namespace CSharpMath.Ios
+{
+  static class IosMathLabels
+  {
+    public static AppleLatexView LatexView(string latex)
+    {
       var view = new AppleLatexView();
       view.SetLatex(latex);
       return view;
---
+++ CSharpMath.Ios/IosMath/IosMathViewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UIKit;
+
+namespace CSharpMath.Ios
+{
+  public class IosMathViewController: UIViewController
+  {
+    public IosMathViewController()
+    {
+    }
+
+    public override void ViewDidLoad()
+    {
+      var view = IosMathLabels.LatexView("x");
+      view.BackgroundColor = UIColor.LightGray;
+      view.SizeToFit();
+      View.Add(view);
+    }
+  }
+}
