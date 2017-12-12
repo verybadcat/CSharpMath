3f6229eb95198d49b002eaabf2dd4b843d3404d7
William Jockusch <jockusch@gmail.com>
2017-12-11 19:35:57 -0600

    Starting on front end. Not seeing AppDelegate in VS on Windows.

--- CSharpMath.Apple/AppleLatexView.cs
+++ CSharpMath.Apple/AppleLatexView.cs
@@ -9,6 +9,7 @@ using CSharpMath.Interfaces;
 using CoreGraphics;
 using UIKit;
 using TGlyph = System.UInt16;
+using CoreText;
 #if __IOS__
 using NView = UIKit.UIView;
 using NColor = UIKit.UIColor;
@@ -16,50 +17,48 @@ using NColor = UIKit.UIColor;
 using NView = AppKit.NSView;
 #endif
 
-namespace CSharpMath.Apple
-{
-    public class AppleLatexView: NView
-    {
-        public void SetMathList(IMathList mathList)
-        {
-            _mathList = mathList;
-            Latex = MathListBuilder.MathListToString(mathList);
-            InvalidateIntrinsicContentSize();
-            SetNeedsLayout();
-        }
-        public void SetLatex(string latex)
-        {
-            Latex = latex;
-            _mathList = MathLists.FromString(latex);
-            InvalidateIntrinsicContentSize();
-            SetNeedsLayout();
-        }
-        public ColumnAlignment TextAlignment { get; set; } = ColumnAlignment.Left;
-
-        private IMathList _mathList;
-
-        public string Latex { get; private set; }
-        private MathListDisplay<TGlyph> _displayList { get; set; }
-
-        public bool DisplayErrorInline { get; set; } = true;
-        public NColor TextColor { get; set; }
-        public AppleLatexView()
-        {
-            Layer.GeometryFlipped = true;
-            BackgroundColor = NColor.Clear;
-            TextColor = NColor.Black;
-        }
-
-        public override void Draw(CGRect rect)
-        {
-            base.Draw(rect);
-            if (_mathList!=null)
-            {
-                var cgContext = UIGraphics.GetCurrentContext();
-                cgContext.SaveState();
-
-                cgContext.RestoreState();
-            }
-        }
+namespace CSharpMath.Apple {
+  public class AppleLatexView : NView {
+    public void SetMathList(IMathList mathList) {
+      _mathList = mathList;
+      Latex = MathListBuilder.MathListToString(mathList);
+      InvalidateIntrinsicContentSize();
+      SetNeedsLayout();
     }
+    public void SetLatex(string latex) {
+      Latex = latex;
+      _mathList = MathLists.FromString(latex);
+      InvalidateIntrinsicContentSize();
+      var uiFont = UIFont.SystemFontOfSize(18);
+      var descriptor = new CTFontDescriptor(uiFont.Name, uiFont.PointSize);
+      var ctFont = new CTFont(descriptor, 18);
+      var typesetting = AppleTypesetters.CreateTypesettingContext(ctFont);
+      _displayList = typesetting.CreateLine(_mathList, new AppleMathFont(uiFont.Name, 18), LineStyle.Display);
+      SetNeedsLayout();
+    }
+    public ColumnAlignment TextAlignment { get; set; } = ColumnAlignment.Left;
+
+    private IMathList _mathList;
+
+    public string Latex { get; private set; }
+    private MathListDisplay<TGlyph> _displayList { get; set; }
+
+    public bool DisplayErrorInline { get; set; } = true;
+    public NColor TextColor { get; set; }
+    public AppleLatexView() {
+      Layer.GeometryFlipped = true;
+      BackgroundColor = NColor.Clear;
+      TextColor = NColor.Black;
+    }
+
+    public override void Draw(CGRect rect) {
+      base.Draw(rect);
+      if (_mathList != null) {
+        var cgContext = UIGraphics.GetCurrentContext();
+        cgContext.SaveState();
+
+        cgContext.RestoreState();
+      }
+    }
+  }
 }
---
+++ CSharpMath.Ios/IosMath/IosMathLabels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpMath.Apple;
+using Foundation;
+using UIKit;
+
+namespace CSharpMath.Ios.IosMath {
+  static class IosMathLabels {
+    public static AppleLatexView LatexView(string latex) {
+      var view = new AppleLatexView();
+      view.SetLatex(latex);
+      return view;
+    }
+  }
+}
