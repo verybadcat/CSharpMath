53ecbdc15bb0361afce80d8207716302739e391f
Happypig375 <hadrianwttang@outlook.com>
2018-02-12 23:39:42 +0800

    Add Forms project Update packages Rename SKImageView to SKImagePainter
    (A 100-change commit!)

---
+++ CSharpMath.Forms/CSharpMath.Forms/FormsLatexView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SkiaSharp.Views.Forms;
+
+namespace CSharpMath.Forms
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FormsLatexView : SKCanvasView
+	{
+    readonly SkiaSharp.SkiaLatexPainter painter = new SkiaSharp.SkiaLatexPainter();
+
+    public FormsLatexView() => InitializeComponent();
+
+    protected override void OnPaintSurface(SKPaintSurfaceEventArgs e) {
+      painter.PaintSurface(e.Surface, e.Info);
+      base.OnPaintSurface(e); 
+    }
+  }
+}
---
+++ CSharpMath.SkiaSharp/SkiaLatexPainter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using SkiaSharp;
+
+namespace CSharpMath.SkiaSharp
+{
+  public class SkiaLatexPainter {
+    public void PaintSurface(SKSurface surface, SKImageInfo imageInfo) { }
+  }
+}
