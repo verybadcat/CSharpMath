08f53e4232f9992d093e8feab387830fa61479c8
Happypig375 <hadrianwttang@outlook.com>
2018-05-26 18:09:26 +0800

    Example running on Android!

--- CSharpMath.Forms.Example/CSharpMath.Forms.Example/CustomExamplePage.xaml.cs
+++ CSharpMath.Forms.Example/CSharpMath.Forms.Example/CustomExamplePage.xaml.cs
@@ -21,7 +21,7 @@ namespace CSharpMath.Forms.Example
         View.FontSize = (float)Size.SelectedItem;
         View.InvalidateSurface();
       };
-      Entry.TextChanged += (sender, e) => Task.Run(() => View.LaTeX = Entry.Text);
+      Entry.TextChanged += (sender, e) => View.LaTeX = Entry.Text;
     }
     protected override void OnDisappearing() {
       //App.AllViews.Remove(View);
