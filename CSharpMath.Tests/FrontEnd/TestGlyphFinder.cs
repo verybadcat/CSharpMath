14881acbb790276a2ef3e7b85c30d7f2269484d1
Happypig375 <hadrianwttang@outlook.com>
2018-06-22 00:34:01 +0800

    Test buildable again

--- CSharpMath.Tests/FrontEnd/TestGlyphFinder.cs
+++ CSharpMath.Tests/FrontEnd/TestGlyphFinder.cs
@@ -1,4 +1,5 @@
-﻿using CSharpMath.FrontEnd;
+﻿using CSharpMath.Display;
+using CSharpMath.FrontEnd;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace CSharpMath.Tests.FrontEnd
 {
-  class TestGlyphFinder : IGlyphFinder<char> {
-    public char FindGlyphForCharacterAtIndex(int index, string str) {
+  class TestGlyphFinder : IGlyphFinder<MathFont<char>, char> {
+    public char FindGlyphForCharacterAtIndex(MathFont<char> font, int index, string str) {
       return str[index];
     }
 
-    public char[] FindGlyphs(string str) => str.ToArray();
+    public char[] FindGlyphs(MathFont<char> font, string str) => str.ToArray();
 
     public string FindStringDebugPurposesOnly(char[] glyphs) => new string(glyphs);
     public bool GlyphIsEmpty(char glyph)
