caf81df20732af77fbb3c0226a926b4c8c4f76a8
Hadrian Tang <hadrianwttang@outlook.com>
2018-07-24 14:41:34 +0800

    Forms.WPF Example, but not working yet

--- CSharpMath.Rendering/Text/TextPainter.cs
+++ CSharpMath.Rendering/Text/TextPainter.cs
@@ -1,21 +1,24 @@
-﻿using CSharpMath.Rendering;
-
-namespace CSharpMath.Rendering {
+﻿namespace CSharpMath.Rendering {
   public abstract class TextPainter<TCanvas, TColor> : Painter<TCanvas, TextSource, TColor> {
     public TextPainter(float fontSize = DefaultFontSize, float lineWidth = DefaultFontSize * 30) : base(fontSize) { }
 
     public float LineWidth { get; set; }
 
     protected override IDisplay<MathFonts, Glyph> CreateDisplay(MathFonts fonts) {
-      var displays = Atom.ToDisplay(fonts, default);
-      float y = 0f, lineHeight = 0f;
+      var atomDisplay = Atom.ToDisplay(fonts, default);
+      float lineHeight = 0f;
       void AddLineBreaks(IDisplay<MathFonts, Glyph> display) {
-        if (displays is Display.MathListDisplay<MathFonts, Glyph> list)
+        if (display is Display.MathListDisplay<MathFonts, Glyph> list)
           foreach (var d in list.Displays) AddLineBreaks(d);
-        else if (display.Position.X >= LineWidth) y += lineHeight;
-
+        else {
+          if (display.Position.X >= LineWidth) {
+            display.Position = new System.Drawing.PointF(0f, display.Position.Y + lineHeight);
+            lineHeight = display.ComputeDisplayBounds().Height;
+          } else lineHeight = System.Math.Max(lineHeight, display.ComputeDisplayBounds().Height);
+        }
       }
-      return displays;
+      AddLineBreaks(atomDisplay);
+      return atomDisplay;
     }
 
     public TextAtom Atom { get => Source.Atom; set => Source = new TextSource(value); }
