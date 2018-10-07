f23b55512069029120c213ac4391f0c92fbacd31
Happypig375 <hadrianwttang@outlook.com>
2018-10-07 19:30:47 +0800

    Ported a little bit more

--- CSharpMath.Editor/MathList.cs
+++ CSharpMath.Editor/MathList.cs
@@ -5,7 +5,14 @@ using System.Text;
 namespace CSharpMath.Editor {
   using Atoms;
   class MathList : IMathList {
-    void InsertAtom(MathAtom atom, int index) {
+    void InsertAtom(MathAtom atom, MathListIndex index) {
+      if (index.AtomIndex > Atoms.Count)
+        throw new IndexOutOfRangeException($"Index {index.AtomIndex} is out of bounds for list of size {Atoms.Count}");
+      switch (index.SubIndexType) {
+        case MathListSubIndexType.None:
+          InsertAtom(atom, index.AtomIndex);
+          break;
+      }
     }
   }
 }
