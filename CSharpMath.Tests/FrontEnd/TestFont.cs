a9c71a1a25b4d370c6febc9030a80d1848d1059d
Happypig375 <hadrianwttang@outlook.com>
2018-10-15 10:40:58 +0800

    Updated TestMathFont to implement IFont instead of IMathFont

--- CSharpMath.Tests/FrontEnd/TestFont.cs
+++ CSharpMath.Tests/FrontEnd/TestFont.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using CSharpMath.Enumerations;
 
 namespace CSharpMath.Tests.FrontEnd {
-  public struct TestMathFont : Display.IMathFont<char> {
+  public struct TestMathFont : Display.IFont<char> {
     public TestMathFont(float pointSize) => PointSize = pointSize;
     public float PointSize { get; }
   }
