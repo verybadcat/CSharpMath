374c2d0dbaf8784be5bfe9b5b5ba45f89c7bd641
Hadrian Tang <hadrianwttang@outlook.com>
2018-12-10 12:03:19 +0800

    KeyInput.cs

---
+++ CSharpMath.Editor/Keyboards/KeyboardInput.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpMath.Editor {
+  public enum KeyboardInput {
+    Up = '↑', Down = '↓', Left = '←', Right = '→', Backspace = '⌫', Clear = '⎚',
+
+    LeftBracket = '(', RightBracket = ')',
+    D0 = '0', D1 = '1', D2 = '2', D3 = '3', D4 = '4', D5 = '5', D6 = '6', D7 = '7', D8 = '8', D9 = '9',
+    Decimal = '.', Plus = '+', Minus = '−', Multiply = '×', Divide = '÷', 
+
+    A = 'a', B = 'b', C = 'c', D = 'd', E = 'e', F = 'f', G = 'g', H = 'h', I = 'i',
+    J = 'j', K = 'k', L = 'l', M = 'm', N = 'n', O = 'o', P = 'p', Q = 'q', R = 'r',
+    S = 's', T = 't', U = 'u', V = 'v', W = 'w', X = 'x', Y = 'y', Z = 'z',
+    
+    LowerA = 'a', LowerB = 'b', LowerC = 'c', LowerD = 'd', LowerE = 'e', LowerF = 'f', LowerG = 'g', LowerH = 'h', LowerI = 'i',
+    LowerJ = 'j', LowerK = 'k', LowerL = 'l', LowerM = 'm', LowerN = 'n', LowerO = 'o', LowerP = 'p', LowerQ = 'q', LowerR = 'r',
+    LowerS = 's', LowerT = 't', LowerU = 'u', LowerV = 'v', LowerW = 'w', LowerX = 'x', LowerY = 'y', LowerZ = 'z',
+
+    Α = 'Α', α = 'α', Β = 'Β', β = 'β', Γ = 'Γ', γ = 'γ', Δ = 'Δ', δ = 'δ', Ε = 'Ε', ε = 'ε', Ζ = 'Ζ', ζ = 'ζ',
+    Η = 'Η', η = 'η', Θ = 'Θ', θ = 'θ', Ι = 'Ι', ι = 'ι', Κ = 'Κ', κ = 'κ', Λ = 'Λ', λ = 'λ', Μ = 'Μ', μ = 'μ',
+    Ν = 'Ν', ν = 'ν', Ξ = 'Ξ', ξ = 'ξ', Ο = 'Ο', ο = 'ο', Π = 'Π', π = 'π', Ρ = 'Ρ', ρ = 'ρ', Σ = 'Σ', σ = 'σ',
+    ς = 'ς', Τ = 'Τ', τ = 'τ', Υ = 'Υ', υ = 'υ', Φ = 'Φ', φ = 'φ', Χ = 'Χ', χ = 'χ', Ω = 'Ω', ω = 'ω',
+
+
+  }
+}
