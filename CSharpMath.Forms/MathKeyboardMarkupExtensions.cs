ab694f7b9c1a4e066e8e550d061af60e27b36875
Hadrian Tang <hadrianwttang@outlook.com>
2018-12-17 13:24:02 +0800

    WIP - More MathKeyboard stuff

--- CSharpMath.Forms/MathKeyboardMarkupExtensions.cs
+++ CSharpMath.Forms/MathKeyboardMarkupExtensions.cs
@@ -5,32 +5,34 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CSharpMath.Forms.MathKeyboardMarkupExtensions {
-  using T = Xamarin.Forms.Command;
-  public abstract class MarkUpExtensionBase<T>: IMarkupExtension<T> {
-    public abstract T ProvideValue();
-    public T ProvideValue(IServiceProvider serviceProvider) => ProvideValue();
-    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) =>
-      ProvideValue(serviceProvider);
-  }
-
   [AcceptEmptyServiceProvider]
-  public class MathInputExtension : MarkUpExtensionBase<Command> {
+  public class MathInputExtension : IMarkupExtension<Command> {
     public Editor.MathKeyboardInput Input { get; set; }
     public Rendering.MathKeyboard Keyboard { get; set; }
 
-    public override Command ProvideValue() =>
+    public Command ProvideValue(IServiceProvider _) =>
       Input is 0 ? throw new ArgumentNullException(nameof(Input)) :
       Keyboard is null ? throw new ArgumentNullException(nameof(Keyboard)) :
       new Command(() => Keyboard.KeyPress(Input));
+    object IMarkupExtension.ProvideValue(IServiceProvider _) => ProvideValue(_);
   }
 
   [AcceptEmptyServiceProvider]
-  public class ToggleVisibilityExtension : MarkUpExtensionBase<Command> {
+  public class ToggleVisibilityExtension : IMarkupExtension<Command> {
     public VisualElement Target { get; set; }
 
-    public override Command ProvideValue() =>
+    public Command ProvideValue(IServiceProvider _) =>
       Target is null ? throw new ArgumentNullException(nameof(Target)) :
       new Command(() => Target.IsVisible ^= true);
+    object IMarkupExtension.ProvideValue(IServiceProvider _) => ProvideValue(_);
   }
   
+  [AcceptEmptyServiceProvider, ContentProperty(nameof(Base))]
+  public class MultiplyExtension : IMarkupExtension<double> {
+    public double Base { get; set; }
+    public double By { get; set; }
+
+    public double ProvideValue(IServiceProvider _) => Base * By;
+    object IMarkupExtension.ProvideValue(IServiceProvider _) => ProvideValue(_);
+  }
 }
