be6b9636c02f2c74f85dfc4320a139754fbd5703
Happypig375 <hadrianwttang@outlook.com>
2018-12-17 20:18:20 +0800

    Measuring GLView + MathPainter memory leak

---
+++ CSharpMath.GLMemoryLeakTest.Isolated/CSharpMath.GLMemoryLeakTest.Isolated/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CSharpMath.GLMemoryLeakTest.Isolated {
+  public partial class MainPage : ContentPage {
+    public MainPage() {
+      InitializeComponent();
+    }
+  }
+}
---
+++ CSharpMath.GLMemoryLeakTest/CSharpMath.GLMemoryLeakTest/Views.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaLeakMinProject
+{
+    public class GLView : SKGLView
+    {
+        public GLView()
+        {
+
+        }
+    }
+
+    public class GLMathView : SKGLView
+    {
+        CSharpMath.SkiaSharp.TextPainter textPainter = new CSharpMath.SkiaSharp.TextPainter() { Text = "$\\int 2x^2 + x - \\frac{a}{b}$", FontSize = 20.0F };
+        public GLMathView()
+        {
+
+        }
+        protected override void OnPaintSurface(SKPaintGLSurfaceEventArgs e)
+        { 
+            textPainter.Draw(e.Surface.Canvas, new System.Drawing.PointF(0.0F, 0.0F), e.BackendRenderTarget.Width);
+        }
+    }
+
+    public class CanvasView : SKCanvasView
+    {
+        public CanvasView()
+        {
+
+        }
+    }
+    public class CanvasMathView : SKCanvasView
+    {
+        CSharpMath.SkiaSharp.TextPainter textPainter = new CSharpMath.SkiaSharp.TextPainter() { Text = "$\\int 2x^2 + x - \\frac{a}{b}$", FontSize = 20.0F };
+        public CanvasMathView()
+        {
+
+        }
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            textPainter.Draw(e.Surface.Canvas, new System.Drawing.PointF(0.0F, 0.0F), e.Info.Width);
+        } 
+    }
+}
