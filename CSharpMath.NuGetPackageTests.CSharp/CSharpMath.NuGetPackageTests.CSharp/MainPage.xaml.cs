1158d89e7f424e803bda7493fc9bfc123c90edb7
Happypig375 <hadrianwttang@outlook.com>
2019-01-06 17:50:31 +0800

    Example usage for https://github.com/verybadcat/CSharpMath/issues/11

--- CSharpMath.NuGetPackageTests.CSharp/CSharpMath.NuGetPackageTests.CSharp/MainPage.xaml.cs
+++ CSharpMath.NuGetPackageTests.CSharp/CSharpMath.NuGetPackageTests.CSharp/MainPage.xaml.cs
@@ -9,10 +9,21 @@ namespace CSharpMath.NuGetPackageTests.CSharp
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
-		{
-			InitializeComponent();
+    public MainPage() {
+      InitializeComponent();
+      global::SkiaSharp.Views.Forms.SKCanvasView canvasView = canvas;
+      CSharpMath.Forms.MathKeyboard keyboard = b;
       
+      var painter = new CSharpMath.SkiaSharp.MathPainter
+      { /*set all properties aside from LocalTypefaces, FontSize, LineStyle,
+          MathList, LaTeX and Source (these are ignored)*/ };
+      keyboard.RedrawRequested += (sender, e) => canvasView.InvalidateSurface();
+      canvasView.PaintSurface += (sender, e) => {
+        e.Surface.Canvas.Clear();
+        //for any DrawDisplay overload, arguments after canvas are the same as Draw
+        CSharpMath.SkiaSharp.MathPainter.DrawDisplay(painter, keyboard.Display, e.Surface.Canvas);
+        keyboard.DrawCaret(e.Surface.Canvas, CSharpMath.Rendering.CaretShape.IBeam);
+      };
 		}
 	}
 }
