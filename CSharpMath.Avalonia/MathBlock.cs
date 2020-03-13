e518038688ee17d8018117a94bd98f78283d8dfb
Hadrian Tang <hadrianwttang@outlook.com>
2020-03-13 18:19:23 +0800

    Buildable again

--- CSharpMath.Avalonia/MathBlock.cs
+++ CSharpMath.Avalonia/MathBlock.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using Avalonia;
 using Avalonia.Media;
@@ -25,15 +26,11 @@ namespace CSharpMath.Avalonia {
     public override void Render(DrawingContext context) =>
         _painter.Draw(new AvaloniaCanvas(context, Bounds.Size));
 
-    protected override Size MeasureOverride(Size availableSize) {
-      var measure = _painter.Measure;
-
-      if (measure.HasValue) {
-        return new Size(measure.Value.Width, measure.Value.Height);
-      }
-
-      return default;
-    }
+    protected override Size MeasureOverride(Size availableSize) => _painter.Measure switch
+    {
+      System.Drawing.RectangleF rect => new Size(rect.Width, rect.Height),
+      null => default
+    };
 
     private void UpdateTypeface(FontFamily family) {
       //throw new NotImplementedException();
