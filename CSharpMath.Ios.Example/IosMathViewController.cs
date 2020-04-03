c8b600802b49f859e56c1db63bc610b33da485e0
Hadrian Tang <hadrianwttang@outlook.com>
2020-04-04 03:01:59 +0800

    Use an actual available choice for LaTeX

--- CSharpMath.Ios.Example/IosMathViewController.cs
+++ CSharpMath.Ios.Example/IosMathViewController.cs
@@ -4,7 +4,7 @@ namespace CSharpMath.Ios.Example {
   public class IosMathViewController : UIViewController {
     public override void ViewDidLoad() {
       View.BackgroundColor = UIColor.White;
-      var latexView = IosMathLabels.MathView(Rendering.Tests.MathData.ColorBox, 50);  // WJWJWJ latex here
+      var latexView = IosMathLabels.MathView(Rendering.Tests.MathData.IntegralColorBoxCorrect, 50);  // WJWJWJ latex here
       latexView.ContentInsets = new UIEdgeInsets(10, 10, 10, 10);
       var size = latexView.SizeThatFits(new CoreGraphics.CGSize(370, 280));
       latexView.Frame = new CoreGraphics.CGRect(0, 40, size.Width, size.Height);
