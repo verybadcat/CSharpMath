bfce31ad3f5fa5bc82dec2ea74d76e84434c9436
Hadrian Tang <hadrianwttang@outlook.com>
2020-04-08 12:56:01 +0800

    Test that asserts all characters can be displayed properly

---
+++ CSharpMath.Rendering.Tests/TestCommandDisplay.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CSharpMath.Rendering.Tests {
+  using BackEnd;
+  public class TestCommandDisplay {
+    public TestCommandDisplay() =>
+      typefaces = Fonts.GlobalTypefaces.ToArray();
+    readonly Typography.OpenFont.Typeface[] typefaces;
+    public static IEnumerable<object[]> AllCommandValues =>
+      Atom.LaTeXSettings.Commands.Values
+      .SelectMany(v => v.Nucleus.EnumerateRunes())
+      .Distinct()
+      .OrderBy(r => r.Value)
+      .Select(rune => new object[] { rune });
+    [Theory]
+    [MemberData(nameof(AllCommandValues))]
+    public void CommandsAreDisplayable(Rune ch) =>
+      Assert.Contains(typefaces, font => font.GetGlyphIndex(ch.Value) != 0);
+  }
+}
