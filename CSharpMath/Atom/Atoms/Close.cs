4a60f645202347a615b4ddecdf83278135ae7d62
Hadrian Tang <hadrianwttang@outlook.com>
2020-06-06 18:08:28 +0800

    Revoke warning

--- CSharpMath/Atom/Atoms/Close.cs
+++ CSharpMath/Atom/Atoms/Close.cs
@@ -3,8 +3,6 @@ namespace CSharpMath.Atom.Atoms {
   public sealed class Close : MathAtom {
     public Close(string nucleus, bool hasCorrespondingOpen = true) : base(nucleus) =>
       HasCorrespondingOpen = hasCorrespondingOpen;
-#warning To remove when doing issue #82
-    [System.Obsolete("Should be removed when doing issue #82")]
     public bool HasCorrespondingOpen { get; }
     public override bool ScriptsAllowed => true;
     public new Close Clone(bool finalize) => (Close)base.Clone(finalize);
