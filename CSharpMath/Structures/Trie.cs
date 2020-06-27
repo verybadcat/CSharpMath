50500f7f6478b8056771d619738ba7aabc9aba97
Hadrian Tang <hadrianwttang@outlook.com>
2020-06-27 22:09:00 +0800

    Trie optimized

--- CSharpMath/Structures/Trie.cs
+++ CSharpMath/Structures/Trie.cs
@@ -7,12 +7,10 @@ using StringPartition = System.ReadOnlyMemory<char>;
 
 namespace CSharpMath {
   partial class Extensions {
-    public static Structures.SplitResult Split(this StringPartition @this, int splitAt) {
-      var head = @this.Slice(0, splitAt);
-      var rest = @this.Slice(splitAt);
-      return new Structures.SplitResult(head, rest);
-    }
-    public static Structures.ZipResult ZipWith(this StringPartition @this, StringPartition other) {
+    public static (StringPartition Head, StringPartition Tail) Split(this StringPartition @this, int splitAt) =>
+      (@this.Slice(0, splitAt), @this.Slice(splitAt));
+    public static (StringPartition CommonHead, StringPartition ThisRest, StringPartition OtherRest)
+      ZipWith(this StringPartition @this, StringPartition other) {
       int splitIndex = 0;
       var thisEnumerator = @this.Span.GetEnumerator();
       var otherEnumerator = other.Span.GetEnumerator();
@@ -23,13 +21,10 @@ namespace CSharpMath {
         splitIndex++;
       }
 
-      var thisSplitted = @this.Split(splitIndex);
-      var otherSplitted = other.Split(splitIndex);
+      var (commonHead, restThis) = @this.Split(splitIndex);
+      var (_, restOther) = other.Split(splitIndex);
 
-      StringPartition commonHead = thisSplitted.Head;
-      StringPartition restThis = thisSplitted.Rest;
-      StringPartition restOther = otherSplitted.Rest;
-      return new Structures.ZipResult(commonHead, restThis, restOther);
+      return (commonHead, restThis, restOther);
     }
   }
 }
@@ -37,12 +32,6 @@ namespace CSharpMath {
 namespace CSharpMath.Structures {
   // Based on https://github.com/gmamaladze/trienet/tree/f0cce5f980d85e445188b3eb025821fcdb740144/TrieNet/_PatriciaTrie
   // Can't use the TrieNet NuGet package because the .NET Standard 2.0 version is not uploaded: https://github.com/gmamaladze/trienet/issues/12
-  public enum MatchKind {
-    ExactMatch,
-    Contains,
-    IsContained,
-    Partial,
-  }
   [Serializable]
   public class PatriciaTrie<TValue> : PatriciaTrieNode<TValue> {
     public PatriciaTrie() : base(
@@ -88,45 +77,38 @@ namespace CSharpMath.Structures {
     protected IEnumerable<PatriciaTrieNode<TValue>> Children() => m_Children.Values;
     protected void AddValue(TValue value) => m_Values.Enqueue(value);
     private protected virtual void Add(StringPartition keyRest, TValue value) {
-      ZipResult zipResult = m_Key.ZipWith(keyRest);
-      switch (zipResult.MatchKind) {
-        case MatchKind.ExactMatch:
+      var (commonHead, thisRest, otherRest) = m_Key.ZipWith(keyRest);
+      switch (thisRest.Length, otherRest.Length) {
+        case (0, 0):
           AddValue(value);
           break;
-        case MatchKind.IsContained:
-          GetOrCreateChild(zipResult.OtherRest, value);
+        case (0, _):
+          GetOrCreateChild(otherRest, value);
           break;
-        case MatchKind.Contains:
-          SplitOne(zipResult, value);
+        case (_, 0): // A method called "SplitOne" in original source
+          var leftChild = new PatriciaTrieNode<TValue>(thisRest, m_Values, m_Children);
+
+          m_Children = new Dictionary<char, PatriciaTrieNode<TValue>>();
+          m_Values = new Queue<TValue>();
+          AddValue(value);
+          m_Key = commonHead;
+
+          m_Children.Add(thisRest.Span[0], leftChild);
           break;
-        case MatchKind.Partial:
-          SplitTwo(zipResult, value);
+        case (_, _): // A method called "SplitTwo" in original source
+          leftChild = new PatriciaTrieNode<TValue>(thisRest, m_Values, m_Children);
+          var rightChild = new PatriciaTrieNode<TValue>(otherRest, value);
+
+          m_Children = new Dictionary<char, PatriciaTrieNode<TValue>>();
+          m_Values = new Queue<TValue>();
+          m_Key = commonHead;
+
+          char leftKey = thisRest.Span[0];
+          m_Children.Add(leftKey, leftChild);
+          char rightKey = otherRest.Span[0];
+          m_Children.Add(rightKey, rightChild);
           break;
       }
-    }
-    private void SplitOne(ZipResult zipResult, TValue value) {
-      var leftChild = new PatriciaTrieNode<TValue>(zipResult.ThisRest, m_Values, m_Children);
-
-      m_Children = new Dictionary<char, PatriciaTrieNode<TValue>>();
-      m_Values = new Queue<TValue>();
-      AddValue(value);
-      m_Key = zipResult.CommonHead;
-
-      m_Children.Add(zipResult.ThisRest.Span[0], leftChild);
-    }
-
-    private void SplitTwo(ZipResult zipResult, TValue value) {
-      var leftChild = new PatriciaTrieNode<TValue>(zipResult.ThisRest, m_Values, m_Children);
-      var rightChild = new PatriciaTrieNode<TValue>(zipResult.OtherRest, value);
-
-      m_Children = new Dictionary<char, PatriciaTrieNode<TValue>>();
-      m_Values = new Queue<TValue>();
-      m_Key = zipResult.CommonHead;
-
-      char leftKey = zipResult.ThisRest.Span[0];
-      m_Children.Add(leftKey, leftChild);
-      char rightKey = zipResult.OtherRest.Span[0];
-      m_Children.Add(rightKey, rightChild);
     }
 
     protected void GetOrCreateChild(StringPartition key, TValue value) {
@@ -162,49 +144,6 @@ namespace CSharpMath.Structures {
     }
 
     public override string ToString() =>
-      $"Key: {m_Key}, Values: {Values().Count()}, Children:{string.Join(";", m_Children.Keys)}";
-  }
-  [Serializable]
-  public readonly struct SplitResult : IEquatable<SplitResult> {
-    public SplitResult(StringPartition head, StringPartition rest) {
-      Head = head;
-      Rest = rest;
-    }
-
-    public StringPartition Rest { get; }
-    public StringPartition Head { get; }
-    public bool Equals(SplitResult other) => Head.Span.SequenceEqual(other.Head.Span) && Rest.Span.SequenceEqual(other.Rest.Span);
-    public override bool Equals(object obj) => obj is SplitResult result && Equals(result);
-    public override int GetHashCode() => unchecked((Head.GetHashCode() * 397) ^ Rest.GetHashCode());
-    public static bool operator ==(SplitResult left, SplitResult right) => left.Equals(right);
-    public static bool operator !=(SplitResult left, SplitResult right) => !(left == right);
-  }
-  [Serializable]
-  [DebuggerDisplay("Head: '{CommonHead}', This: '{ThisRest}', Other: '{OtherRest}', Kind: {MatchKind}")]
-  public readonly struct ZipResult : IEquatable<ZipResult> {
-    public ZipResult(StringPartition commonHead, StringPartition thisRest, StringPartition otherRest) {
-      CommonHead = commonHead;
-      ThisRest = thisRest;
-      OtherRest = otherRest;
-    }
-    public MatchKind MatchKind =>
-      (ThisRest.Length, OtherRest.Length) switch
-      {
-        (0, 0) => MatchKind.ExactMatch,
-        (0, _) => MatchKind.IsContained,
-        (_, 0) => MatchKind.Contains,
-        (_, _) => MatchKind.Partial,
-      };
-    public StringPartition OtherRest { get; }
-    public StringPartition ThisRest { get; }
-    public StringPartition CommonHead { get; }
-    public bool Equals(ZipResult other) =>
-      CommonHead.Span.SequenceEqual(other.CommonHead.Span)
-      && OtherRest.Span.SequenceEqual(other.OtherRest.Span)
-      && ThisRest.Span.SequenceEqual(other.ThisRest.Span);
-    public override bool Equals(object obj) => obj is ZipResult result && Equals(result);
-    public override int GetHashCode() => (CommonHead, OtherRest, ThisRest).GetHashCode();
-    public static bool operator ==(ZipResult left, ZipResult right) => left.Equals(right);
-    public static bool operator !=(ZipResult left, ZipResult right) => !(left == right);
+      $"Key: {m_Key}, Values: {Values().Count()}, Children: {string.Join(";", m_Children.Keys)}";
   }
 }
