202f7f17c923ffa7d53d88a100a7fc062eb4d066
Hadrian Tang <hadrianwttang@outlook.com>
2020-07-23 00:05:26 +0800

    Apply suggested change
    Co-authored-by: FoggyFinder <FoggyFinder@yandex.ua>

--- CSharpMath/Structures/Dictionary.cs
+++ CSharpMath/Structures/Dictionary.cs
@@ -50,7 +50,7 @@ namespace CSharpMath.Structures {
   ///  A dictionary-based helper where the keys are classes of LaTeX <see cref="string"/>s, with special treatment
   ///  for commands (starting with "\"). The start of an inputted <see cref="Span{T}"/> with type argument
   ///  <see cref="char"/> is parsed, and an arbitrary object <typeparamref name="TValue"/> is returned,
-  ///  along with the number of matching characters. Processing is based on dictionary lookup with fallack
+  ///  along with the number of matching characters. Processing is based on dictionary lookup with fallback
   ///  to specified default functions for command and non-commands when lookup fails.
   ///  For non-commands, dictionary lookup finds the longest matching non-command.
   /// </summary>
