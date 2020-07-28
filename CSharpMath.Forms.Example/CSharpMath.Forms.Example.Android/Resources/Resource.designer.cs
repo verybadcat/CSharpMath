567470596e63b1ed0b23e723b15bd60607a24562
Hadrian Tang <hadrianwttang@outlook.com>
2020-07-28 17:13:02 +0800

    Update

--- CSharpMath.Forms.Example/CSharpMath.Forms.Example.Android/Resources/Resource.designer.cs
+++ CSharpMath.Forms.Example/CSharpMath.Forms.Example.Android/Resources/Resource.designer.cs
@@ -25,108 +25,6 @@ namespace CSharpMath.Forms.Example.Android
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Essentials.Resource.Attribute.font = global::CSharpMath.Forms.Example.Android.Resource.Attribute.font;
-			global::Xamarin.Essentials.Resource.Attribute.fontProviderAuthority = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontProviderAuthority;
-			global::Xamarin.Essentials.Resource.Attribute.fontProviderCerts = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontProviderCerts;
-			global::Xamarin.Essentials.Resource.Attribute.fontProviderFetchStrategy = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontProviderFetchStrategy;
-			global::Xamarin.Essentials.Resource.Attribute.fontProviderFetchTimeout = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontProviderFetchTimeout;
-			global::Xamarin.Essentials.Resource.Attribute.fontProviderPackage = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontProviderPackage;
-			global::Xamarin.Essentials.Resource.Attribute.fontProviderQuery = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontProviderQuery;
-			global::Xamarin.Essentials.Resource.Attribute.fontStyle = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontStyle;
-			global::Xamarin.Essentials.Resource.Attribute.fontWeight = global::CSharpMath.Forms.Example.Android.Resource.Attribute.fontWeight;
-			global::Xamarin.Essentials.Resource.Boolean.abc_action_bar_embed_tabs = global::CSharpMath.Forms.Example.Android.Resource.Boolean.abc_action_bar_embed_tabs;
-			global::Xamarin.Essentials.Resource.Color.notification_action_color_filter = global::CSharpMath.Forms.Example.Android.Resource.Color.notification_action_color_filter;
-			global::Xamarin.Essentials.Resource.Color.notification_icon_bg_color = global::CSharpMath.Forms.Example.Android.Resource.Color.notification_icon_bg_color;
-			global::Xamarin.Essentials.Resource.Color.ripple_material_light = global::CSharpMath.Forms.Example.Android.Resource.Color.ripple_material_light;
-			global::Xamarin.Essentials.Resource.Color.secondary_text_default_material_light = global::CSharpMath.Forms.Example.Android.Resource.Color.secondary_text_default_material_light;
-			global::Xamarin.Essentials.Resource.Dimension.compat_button_inset_horizontal_material = global::CSharpMath.Forms.Example.Android.Resource.Dimension.compat_button_inset_horizontal_material;
-			global::Xamarin.Essentials.Resource.Dimension.compat_button_inset_vertical_material = global::CSharpMath.Forms.Example.Android.Resource.Dimension.compat_button_inset_vertical_material;
-			global::Xamarin.Essentials.Resource.Dimension.compat_button_padding_horizontal_material = global::CSharpMath.Forms.Example.Android.Resource.Dimension.compat_button_padding_horizontal_material;
-			global::Xamarin.Essentials.Resource.Dimension.compat_button_padding_vertical_material = global::CSharpMath.Forms.Example.Android.Resource.Dimension.compat_button_padding_vertical_material;
-			global::Xamarin.Essentials.Resource.Dimension.compat_control_corner_material = global::CSharpMath.Forms.Example.Android.Resource.Dimension.compat_control_corner_material;
-			global::Xamarin.Essentials.Resource.Dimension.notification_action_icon_size = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_action_icon_size;
-			global::Xamarin.Essentials.Resource.Dimension.notification_action_text_size = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_action_text_size;
-			global::Xamarin.Essentials.Resource.Dimension.notification_big_circle_margin = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_big_circle_margin;
-			global::Xamarin.Essentials.Resource.Dimension.notification_content_margin_start = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_content_margin_start;
-			global::Xamarin.Essentials.Resource.Dimension.notification_large_icon_height = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_large_icon_height;
-			global::Xamarin.Essentials.Resource.Dimension.notification_large_icon_width = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_large_icon_width;
-			global::Xamarin.Essentials.Resource.Dimension.notification_main_column_padding_top = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_main_column_padding_top;
-			global::Xamarin.Essentials.Resource.Dimension.notification_media_narrow_margin = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_media_narrow_margin;
-			global::Xamarin.Essentials.Resource.Dimension.notification_right_icon_size = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_right_icon_size;
-			global::Xamarin.Essentials.Resource.Dimension.notification_right_side_padding_top = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_right_side_padding_top;
-			global::Xamarin.Essentials.Resource.Dimension.notification_small_icon_background_padding = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_small_icon_background_padding;
-			global::Xamarin.Essentials.Resource.Dimension.notification_small_icon_size_as_large = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_small_icon_size_as_large;
-			global::Xamarin.Essentials.Resource.Dimension.notification_subtext_size = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_subtext_size;
-			global::Xamarin.Essentials.Resource.Dimension.notification_top_pad = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_top_pad;
-			global::Xamarin.Essentials.Resource.Dimension.notification_top_pad_large_text = global::CSharpMath.Forms.Example.Android.Resource.Dimension.notification_top_pad_large_text;
-			global::Xamarin.Essentials.Resource.Drawable.notification_action_background = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_action_background;
-			global::Xamarin.Essentials.Resource.Drawable.notification_bg = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_bg;
-			global::Xamarin.Essentials.Resource.Drawable.notification_bg_low = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_bg_low;
-			global::Xamarin.Essentials.Resource.Drawable.notification_bg_low_normal = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_bg_low_normal;
-			global::Xamarin.Essentials.Resource.Drawable.notification_bg_low_pressed = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_bg_low_pressed;
-			global::Xamarin.Essentials.Resource.Drawable.notification_bg_normal = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_bg_normal;
-			global::Xamarin.Essentials.Resource.Drawable.notification_bg_normal_pressed = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_bg_normal_pressed;
-			global::Xamarin.Essentials.Resource.Drawable.notification_icon_background = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_icon_background;
-			global::Xamarin.Essentials.Resource.Drawable.notification_template_icon_bg = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_template_icon_bg;
-			global::Xamarin.Essentials.Resource.Drawable.notification_template_icon_low_bg = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_template_icon_low_bg;
-			global::Xamarin.Essentials.Resource.Drawable.notification_tile_bg = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notification_tile_bg;
-			global::Xamarin.Essentials.Resource.Drawable.notify_panel_notification_icon_bg = global::CSharpMath.Forms.Example.Android.Resource.Drawable.notify_panel_notification_icon_bg;
-			global::Xamarin.Essentials.Resource.Id.actions = global::CSharpMath.Forms.Example.Android.Resource.Id.actions;
-			global::Xamarin.Essentials.Resource.Id.action_container = global::CSharpMath.Forms.Example.Android.Resource.Id.action_container;
-			global::Xamarin.Essentials.Resource.Id.action_divider = global::CSharpMath.Forms.Example.Android.Resource.Id.action_divider;
-			global::Xamarin.Essentials.Resource.Id.action_image = global::CSharpMath.Forms.Example.Android.Resource.Id.action_image;
-			global::Xamarin.Essentials.Resource.Id.action_text = global::CSharpMath.Forms.Example.Android.Resource.Id.action_text;
-			global::Xamarin.Essentials.Resource.Id.async = global::CSharpMath.Forms.Example.Android.Resource.Id.async;
-			global::Xamarin.Essentials.Resource.Id.blocking = global::CSharpMath.Forms.Example.Android.Resource.Id.blocking;
-			global::Xamarin.Essentials.Resource.Id.chronometer = global::CSharpMath.Forms.Example.Android.Resource.Id.chronometer;
-			global::Xamarin.Essentials.Resource.Id.forever = global::CSharpMath.Forms.Example.Android.Resource.Id.forever;
-			global::Xamarin.Essentials.Resource.Id.icon = global::CSharpMath.Forms.Example.Android.Resource.Id.icon;
-			global::Xamarin.Essentials.Resource.Id.icon_group = global::CSharpMath.Forms.Example.Android.Resource.Id.icon_group;
-			global::Xamarin.Essentials.Resource.Id.info = global::CSharpMath.Forms.Example.Android.Resource.Id.info;
-			global::Xamarin.Essentials.Resource.Id.italic = global::CSharpMath.Forms.Example.Android.Resource.Id.italic;
-			global::Xamarin.Essentials.Resource.Id.line1 = global::CSharpMath.Forms.Example.Android.Resource.Id.line1;
-			global::Xamarin.Essentials.Resource.Id.line3 = global::CSharpMath.Forms.Example.Android.Resource.Id.line3;
-			global::Xamarin.Essentials.Resource.Id.normal = global::CSharpMath.Forms.Example.Android.Resource.Id.normal;
-			global::Xamarin.Essentials.Resource.Id.notification_background = global::CSharpMath.Forms.Example.Android.Resource.Id.notification_background;
-			global::Xamarin.Essentials.Resource.Id.notification_main_column = global::CSharpMath.Forms.Example.Android.Resource.Id.notification_main_column;
-			global::Xamarin.Essentials.Resource.Id.notification_main_column_container = global::CSharpMath.Forms.Example.Android.Resource.Id.notification_main_column_container;
-			global::Xamarin.Essentials.Resource.Id.right_icon = global::CSharpMath.Forms.Example.Android.Resource.Id.right_icon;
-			global::Xamarin.Essentials.Resource.Id.right_side = global::CSharpMath.Forms.Example.Android.Resource.Id.right_side;
-			global::Xamarin.Essentials.Resource.Id.tag_transition_group = global::CSharpMath.Forms.Example.Android.Resource.Id.tag_transition_group;
-			global::Xamarin.Essentials.Resource.Id.text = global::CSharpMath.Forms.Example.Android.Resource.Id.text;
-			global::Xamarin.Essentials.Resource.Id.text2 = global::CSharpMath.Forms.Example.Android.Resource.Id.text2;
-			global::Xamarin.Essentials.Resource.Id.time = global::CSharpMath.Forms.Example.Android.Resource.Id.time;
-			global::Xamarin.Essentials.Resource.Id.title = global::CSharpMath.Forms.Example.Android.Resource.Id.title;
-			global::Xamarin.Essentials.Resource.Integer.status_bar_notification_info_maxnum = global::CSharpMath.Forms.Example.Android.Resource.Integer.status_bar_notification_info_maxnum;
-			global::Xamarin.Essentials.Resource.Layout.notification_action = global::CSharpMath.Forms.Example.Android.Resource.Layout.notification_action;
-			global::Xamarin.Essentials.Resource.Layout.notification_action_tombstone = global::CSharpMath.Forms.Example.Android.Resource.Layout.notification_action_tombstone;
-			global::Xamarin.Essentials.Resource.Layout.notification_template_custom_big = global::CSharpMath.Forms.Example.Android.Resource.Layout.notification_template_custom_big;
-			global::Xamarin.Essentials.Resource.Layout.notification_template_icon_group = global::CSharpMath.Forms.Example.Android.Resource.Layout.notification_template_icon_group;
-			global::Xamarin.Essentials.Resource.Layout.notification_template_part_chronometer = global::CSharpMath.Forms.Example.Android.Resource.Layout.notification_template_part_chronometer;
-			global::Xamarin.Essentials.Resource.Layout.notification_template_part_time = global::CSharpMath.Forms.Example.Android.Resource.Layout.notification_template_part_time;
-			global::Xamarin.Essentials.Resource.String.status_bar_notification_info_overflow = global::CSharpMath.Forms.Example.Android.Resource.String.status_bar_notification_info_overflow;
-			global::Xamarin.Essentials.Resource.Style.TextAppearance_Compat_Notification = global::CSharpMath.Forms.Example.Android.Resource.Style.TextAppearance_Compat_Notification;
-			global::Xamarin.Essentials.Resource.Style.TextAppearance_Compat_Notification_Info = global::CSharpMath.Forms.Example.Android.Resource.Style.TextAppearance_Compat_Notification_Info;
-			global::Xamarin.Essentials.Resource.Style.TextAppearance_Compat_Notification_Line2 = global::CSharpMath.Forms.Example.Android.Resource.Style.TextAppearance_Compat_Notification_Line2;
-			global::Xamarin.Essentials.Resource.Style.TextAppearance_Compat_Notification_Time = global::CSharpMath.Forms.Example.Android.Resource.Style.TextAppearance_Compat_Notification_Time;
-			global::Xamarin.Essentials.Resource.Style.TextAppearance_Compat_Notification_Title = global::CSharpMath.Forms.Example.Android.Resource.Style.TextAppearance_Compat_Notification_Title;
-			global::Xamarin.Essentials.Resource.Style.Widget_Compat_NotificationActionContainer = global::CSharpMath.Forms.Example.Android.Resource.Style.Widget_Compat_NotificationActionContainer;
-			global::Xamarin.Essentials.Resource.Style.Widget_Compat_NotificationActionText = global::CSharpMath.Forms.Example.Android.Resource.Style.Widget_Compat_NotificationActionText;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont_android_font = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont_android_font;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont_android_fontStyle = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont_android_fontStyle;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont_android_fontWeight = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont_android_fontWeight;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont_font = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont_font;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont_fontStyle = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont_fontStyle;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamilyFont_fontWeight = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamilyFont_fontWeight;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily_fontProviderAuthority = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily_fontProviderAuthority;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily_fontProviderCerts = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily_fontProviderCerts;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily_fontProviderFetchStrategy = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily_fontProviderFetchStrategy;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily_fontProviderFetchTimeout = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily_fontProviderFetchTimeout;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily_fontProviderPackage = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily_fontProviderPackage;
-			global::Xamarin.Essentials.Resource.Styleable.FontFamily_fontProviderQuery = global::CSharpMath.Forms.Example.Android.Resource.Styleable.FontFamily_fontProviderQuery;
-			global::Xamarin.Essentials.Resource.Xml.xamarin_essentials_fileprovider_file_paths = global::CSharpMath.Forms.Example.Android.Resource.Xml.xamarin_essentials_fileprovider_file_paths;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_in = global::CSharpMath.Forms.Example.Android.Resource.Animation.abc_fade_in;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_out = global::CSharpMath.Forms.Example.Android.Resource.Animation.abc_fade_out;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_grow_fade_in_from_bottom = global::CSharpMath.Forms.Example.Android.Resource.Animation.abc_grow_fade_in_from_bottom;
@@ -9680,22 +9578,6 @@ namespace CSharpMath.Forms.Example.Android
 			}
 			
 			private Styleable()
-			{
-			}
-		}
-		
-		public partial class Xml
-		{
-			
-			// aapt resource value: 0x7F100000
-			public const int xamarin_essentials_fileprovider_file_paths = 2131755008;
-			
-			static Xml()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Xml()
 			{
 			}
 		}
