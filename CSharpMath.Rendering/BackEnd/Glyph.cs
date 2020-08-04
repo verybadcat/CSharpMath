0aba498e6e74f30c4bfc962e812767cf901486ae
Charles Roddie <charles.roddie@mathspire.com>
2020-08-04 21:28:54 +0100

    remove unused deconstruct and commented code

--- CSharpMath.Rendering/BackEnd/Glyph.cs
+++ CSharpMath.Rendering/BackEnd/Glyph.cs
@@ -1,21 +1,12 @@
 namespace CSharpMath.Rendering.BackEnd {
 
   public readonly struct Glyph {
-    //public enum FontStyle {
-    //  Normal = 0,
-    //  Bold = 1,
-    //  Italic = 2,
-    //  BoldItalic = 3
-    //}
     public Glyph(Typography.OpenFont.Typeface typeface, Typography.OpenFont.Glyph info) {
       Typeface = typeface ?? throw new System.ArgumentNullException(nameof(typeface));
       Info = info ?? throw new System.ArgumentNullException(nameof(info));
     }
     public Typography.OpenFont.Typeface Typeface { get; }
     public Typography.OpenFont.Glyph Info { get; }
-    public void Deconstruct
-      (out Typography.OpenFont.Typeface typeface, out Typography.OpenFont.Glyph glyph)
-      { typeface = Typeface; glyph = Info; }
     public bool IsEmpty => Equals(Empty);
     public static readonly Glyph Empty = new Glyph();
   }
