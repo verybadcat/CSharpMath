ea1643c638fa76f4c8992dbae5b71bce7497c7db
Charles Roddie <charles.roddie@mathspire.com>
2020-08-04 22:43:53 +0100

    Document Typefaces

--- CSharpMath.Rendering/BackEnd/Typefaces.cs
+++ CSharpMath.Rendering/BackEnd/Typefaces.cs
@@ -5,14 +5,19 @@ using System.Linq;
 using Typography.OpenFont;
 
 namespace CSharpMath.Rendering.BackEnd {
+  /// <summary>Typefaces stored in order of precedence.</summary>
   public class Typefaces : IEnumerable<Typeface> {
     internal Typefaces(Typeface _default) =>
       _typefaces = new Dictionary<sbyte, Typeface> { [0] = _default };
     private readonly IDictionary<sbyte, Typeface> _typefaces;
+    /// <summary>Adds typeface at highest precedence.</summary>
+    /// <param name="item"></param>
     public void AddOverride(Typeface item) =>
       _typefaces.Add(checked((sbyte)(_typefaces.Keys.Min() - 1)), item);
+    /// <summary>Adds typeface at lowest precedence.</summary>
     public void AddSupplement(Typeface item) =>
       _typefaces.Add(checked((sbyte)(_typefaces.Keys.Max() + 1)), item);
+    /// <summary>Gets typefaces, from highest to lowest precedence.</summary>
     public IEnumerator<Typeface> GetEnumerator() =>
       _typefaces.OrderBy(p => p.Key).Select(p => p.Value).GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
