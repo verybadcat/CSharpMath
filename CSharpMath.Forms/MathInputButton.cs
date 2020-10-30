da8b2913582546a21ecf8256c928e580095bc963
SymboLinker <SymboLinker@gmail.com>
2020-10-30 16:14:00 +0100

    Delete NullableColorBindablePropertyHelper (I am afraid I did not try the right things before - it just works)

--- CSharpMath.Forms/MathInputButton.cs
+++ CSharpMath.Forms/MathInputButton.cs
@@ -12,15 +12,15 @@ namespace CSharpMath.Forms {
     public MathKeyboardInput Input { get => (MathKeyboardInput)GetValue(InputProperty); set => SetValue(InputProperty, value); }
     public static readonly BindableProperty InputProperty = BindablePropertyWithButtonDraw(nameof(Input), typeof(MathKeyboardInput));
     public Color? PlaceholderActiveColor {
-      get => this.GetNullableColor(PlaceholderActiveColorProperty);
-      set => this.SetNullableColor(PlaceholderActiveColorProperty, value);
+      get => (Color?)GetValue(PlaceholderActiveColorProperty);
+      set => SetValue(PlaceholderActiveColorProperty, value);
     }
-    public static readonly BindableProperty PlaceholderActiveColorProperty = BindablePropertyWithButtonDraw(nameof(PlaceholderActiveColor), typeof(Color), defaultValue: NullableColorBindablePropertyHelper.Null);
+    public static readonly BindableProperty PlaceholderActiveColorProperty = BindablePropertyWithButtonDraw(nameof(PlaceholderActiveColor), typeof(Color?), defaultValue: null);
     public Color? PlaceholderRestingColor {
-      get => this.GetNullableColor(PlaceholderRestingColorProperty);
-      set => this.SetNullableColor(PlaceholderRestingColorProperty, value);
+      get => (Color?)GetValue(PlaceholderRestingColorProperty);
+      set => SetValue(PlaceholderRestingColorProperty, value);
     }
-    public static readonly BindableProperty PlaceholderRestingColorProperty = BindablePropertyWithButtonDraw(nameof(PlaceholderRestingColor), typeof(Color), defaultValue: NullableColorBindablePropertyHelper.Null);
+    public static readonly BindableProperty PlaceholderRestingColorProperty = BindablePropertyWithButtonDraw(nameof(PlaceholderRestingColor), typeof(Color?), defaultValue: null);
     static BindableProperty BindablePropertyWithButtonDraw(string propertyName, Type propertyType, object? defaultValue = null) =>
       BindableProperty.Create(propertyName, propertyType, typeof(MathInputButton), defaultValue: defaultValue, propertyChanged: (b, o, n) => ((MathInputButton)b).ButtonDraw());
     protected override void ButtonDraw() {
@@ -72,13 +72,5 @@ namespace CSharpMath.Forms {
     public static readonly BindableProperty KeyboardProperty = BindableProperty.Create(nameof(Keyboard), typeof(MathKeyboard), typeof(ImageSourceMathInputButton));
     public MathKeyboardInput Input { get => (MathKeyboardInput)GetValue(InputProperty); set => SetValue(InputProperty, value); }
     public static readonly BindableProperty InputProperty = BindableProperty.Create(nameof(Input), typeof(MathKeyboardInput), typeof(ImageSourceMathInputButton));
-  }
-  public static class NullableColorBindablePropertyHelper {
-    public readonly static Color Null = Color.Transparent;
-    public static Color? GetNullableColor(this BindableObject bindableObject, BindableProperty bindableProperty) {
-      var v = (Color)bindableObject.GetValue(bindableProperty);
-      return v == Null ? (Color?)null : v;
-    }
-    public static void SetNullableColor(this BindableObject bindableObject, BindableProperty bindableProperty, Color? value) => bindableObject.SetValue(bindableProperty, value ?? Null);
   }
 }
