3aecfcc55bbfe29597e28f3201bcd37920a09307
Adam Drzewiecki <adam_d_1998@tlen.pl>
2024-01-16 20:39:35 +0100

    .net Maui example

---
+++ CSharpMath.Maui.Example/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpMath.Maui.Example {
+  public partial class App : Application {
+    public App() {
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
---
+++ CSharpMath.Maui.Example/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace CSharpMath.Maui.Example {
+  [Register("AppDelegate")]
+  public class AppDelegate : MauiUIApplicationDelegate {
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+  }
+}
