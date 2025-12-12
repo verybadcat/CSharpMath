42d240ae3f542c2b88209060296a9faa8b147be4
Charles Roddie <charles.roddie@summatic.co.uk>
2025-12-12 04:23:49 +0000

    try putting the input in as a manual handler

--- CSharpMath.Rendering/Text/TextLaTeXParser.cs
+++ CSharpMath.Rendering/Text/TextLaTeXParser.cs
@@ -52,10 +52,11 @@ BreakText(@"Here are some text $1 + 12 \frac23 \sqrt4$ $$Display$$ text")
       int dollarCount = 0;
       var globalAtoms = new TextAtomListBuilder();
       List<BreakAtInfo> breakList = new List<BreakAtInfo>(); // Roslyn bug that assumes breakList is nullable resulting in warnings so var is not used
-      var breaker = new CustomBreaker() { // v => breakList.Add(new BreakAtInfo(v.LatestBreakAt, v.LatestWordKind)) // The CustomBreaker constructor no longer takes an argument
+      var breaker = new CustomBreaker() {
         BreakNumberAfterText = true,
         ThrowIfCharOutOfRange = false
       };
+      breaker.SetNewBreakHandler(v => breakList.Add(new BreakAtInfo(v.LatestBreakAt, v.LatestWordKind)));
       foreach (var engine in AdditionalBreakingEngines)
         breaker.AddBreakingEngine(engine);
       breaker.BreakWords(latexSource);
