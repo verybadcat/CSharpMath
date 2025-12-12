35845527ed15d6a3a32f1fcffa16bef2ec623309
Charles Roddie <charles.roddie@summatic.co.uk>
2025-12-12 11:23:47 +0000

    Remove ios projects

--- CSharpMath.Apple/BackEnd/IGlyphNameProvider.cs
+++
@@ -1,9 +0,0 @@
-namespace CSharpMath.Display.FrontEnd {
-  /// <remarks>
-  /// The names provided by this class are used to lookup spacings in JsonMathTable.cs.
-  /// </remarks>
-  public interface IGlyphNameProvider<TGlyph> {
-    string GetGlyphName(TGlyph glyph);
-    TGlyph GetGlyph(string glyphName);
-  }
-}
--- CSharpMath.Ios/IosMathLabels.cs
+++
@@ -1,7 +0,0 @@
-namespace CSharpMath.Ios {
-  using Apple;
-  public static class IosMathLabels {
-    public static AppleMathView MathView(string latex, float fontSize) =>
-      new AppleMathView(AppleTypesetters.LatinMath, fontSize) { LaTeX = latex };
-  }
-}
