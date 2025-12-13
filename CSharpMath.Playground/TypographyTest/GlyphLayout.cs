61f08fdd75b76e4b9d737b0cdd31183fa7577393
Charles Roddie <charles.roddie@summatic.co.uk>
2025-12-13 13:54:23 +0000

    try claude fix

--- CSharpMath.Playground/TypographyTest/GlyphLayout.cs
+++ CSharpMath.Playground/TypographyTest/GlyphLayout.cs
@@ -2,10 +2,15 @@ using System.Linq;
 
 namespace CSharpMath.Playground.TypographyTest {
   static class GlyphLayout {
-    public static Typography.TextLayout.GlyphLayout Get =>
-      new Typography.TextLayout.GlyphLayout(new Typography.OpenFont.OpenFontReader()
+    public static Typography.TextLayout.GlyphLayout Get {
+      get {
+        var layout = new Typography.TextLayout.GlyphLayout();
+        layout.Typeface = new Typography.OpenFont.OpenFontReader()
           .Read(new System.IO.FileStream(
             System.IO.Directory.EnumerateFiles(Paths.ReferenceFontsFolder, "*.otf").First(),
-            System.IO.FileMode.Open)) ?? throw new System.InvalidOperationException("Invalid font!"));
+            System.IO.FileMode.Open)) ?? throw new System.InvalidOperationException("Invalid font!");
+        return layout;
+      }
+    }
   };
 }
