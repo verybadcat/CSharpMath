55ced856cd5a692f77cc688efea539212884d3c1
Charles Roddie <charles.roddie@summatic.co.uk>
2025-12-13 13:10:58 +0000

    claude build fix

--- CSharpMath.Playground/TypographyTest/BreakString.cs
+++ CSharpMath.Playground/TypographyTest/BreakString.cs
@@ -7,13 +7,16 @@ namespace CSharpMath.Playground.TypographyTest {
       Console.OutputEncoding = Encoding.Unicode;
       const int length = 1000000;
       var s = new System.Diagnostics.Stopwatch();
-      var b = new Typography.TextBreak.CustomBreaker(_ => { });
-      b.BreakWords("Initialize"); //Don't measure startup costs
+      var b = new Typography.TextBreak.CustomBreaker();
+      var init = "Initialize".ToCharArray();
+      b.BreakWords(init, 0, init.Length); //Don't measure startup costs
       foreach (var c in new[] {
         '0', '3', ' ', 'a', 'r', '#', '.', '%', '\r', '\u3232', '\uFEFF', '0'
       }) {
         s.Restart();
-        b.BreakWords(new string(c, length));
+        var str = new string(c, length);
+        var charArray = str.ToCharArray();
+        b.BreakWords(charArray, 0, charArray.Length);
         s.Stop();
         Console.WriteLine("'{0}': {1}", c, s.Elapsed);
         GC.Collect();
