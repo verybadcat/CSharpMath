aa92dd2bcede9132b4e80ae1dce6096ab51f2533
Charles Roddie <charles.roddie@summatic.co.uk>
2025-12-14 06:32:35 +0000

    comment out avalonia tests

--- CSharpMath.Rendering.Tests/TestRendering_Avalonia.cs
+++ CSharpMath.Rendering.Tests/TestRendering_Avalonia.cs
@@ -1,14 +1,14 @@
-using AvaloniaColor = Avalonia.Media.Color;
+//using AvaloniaColor = Avalonia.Media.Color;
 
-namespace CSharpMath.Rendering.Tests {
-  using System.IO;
-  using Avalonia;
-  using FrontEnd;
-  public class TestRendering_Avalonia : TestRendering<AvaloniaCanvas, AvaloniaColor, MathPainter, TextPainter> {
-    protected override string FrontEnd => nameof(Avalonia);
-    protected override double FileSizeTolerance => 0.0472;
-    protected override void DrawToStream<TContent>(Painter<AvaloniaCanvas, TContent, AvaloniaColor> painter,
-      Stream stream, float textPainterCanvasWidth, TextAlignment alignment) =>
-      painter.DrawAsPng(stream, textPainterCanvasWidth, alignment);
-  }
-}
+//namespace CSharpMath.Rendering.Tests {
+//  using System.IO;
+//  using Avalonia;
+//  using FrontEnd;
+//  public class TestRendering_Avalonia : TestRendering<AvaloniaCanvas, AvaloniaColor, MathPainter, TextPainter> {
+//    protected override string FrontEnd => nameof(Avalonia);
+//    protected override double FileSizeTolerance => 0.0472;
+//    protected override void DrawToStream<TContent>(Painter<AvaloniaCanvas, TContent, AvaloniaColor> painter,
+//      Stream stream, float textPainterCanvasWidth, TextAlignment alignment) =>
+//      painter.DrawAsPng(stream, textPainterCanvasWidth, alignment);
+//  }
+//}
