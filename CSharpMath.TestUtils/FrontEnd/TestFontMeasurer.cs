52f20cfc7c74ba0fa008b388b47dfa8ce85b52ee
Hadrian Tang <hadrianwttang@outlook.com>
2026-01-06 05:01:35 +0800

    Further simplification

--- CSharpMath.TestUtils/FrontEnd/TestFontMeasurer.cs
+++
@@ -1,9 +0,0 @@
-namespace CSharpMath.TestUtils;
-
-using TGlyph = System.Text.Rune;
-
-public class TestFontMeasurer : IFontMeasurer<TestFont, TGlyph> {
-  TestFontMeasurer() { }
-  public static TestFontMeasurer Instance { get; } = new TestFontMeasurer();
-  public int GetUnitsPerEm(TestFont font) => 1000;
-}
