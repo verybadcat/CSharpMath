77540f26478beb406028323735207ac41092fae4
Hadrian Tang <hadrianwttang@outlook.com>
2026-01-28 00:30:28 +0800

    TextBlock experiment

--- CSharpMath.Uno.Example/Pages/TextPage.xaml.cs
+++ CSharpMath.Uno.Example/Pages/TextPage.xaml.cs
@@ -7,9 +7,9 @@ public sealed partial class TextPage : Page {
   public float[] FontSizes { get; } = TryPage.FontSizes;
   public TextPage() {
     InitializeComponent();
-    FontSizer.SelectedItem = 30f;
+    //FontSizer.SelectedItem = 30f;
     // Unlike MAUI and Avalonia, {Binding}s and {x:Bind}s are cleared when a value is assigned.
     // Since LaTeX property automatically re-formats assigned input, we need to bind in the code behind.
-    Text.RegisterPropertyChangedCallback(TextBox.TextProperty, (sender, dp) => View.LaTeX = Text.Text);
+    //Text.RegisterPropertyChangedCallback(TextBox.TextProperty, (sender, dp) => View.LaTeX = Text.Text);
   }
 }
