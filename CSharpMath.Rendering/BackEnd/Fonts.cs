f8fc599e4882c1e9d2076e5d0e50c9ab5c9ff46e
Hadrian Tang <hadrianwttang@outlook.com>
2026-08-27 17:04:10 +0800

    fix: update ink-aware rendering baselines and API metadata

--- CSharpMath.Rendering/BackEnd/Fonts.cs
+++ CSharpMath.Rendering/BackEnd/Fonts.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Drawing;
+using System.Linq;
 using Typography.OpenFont;
 using Typography.OpenFont.Extensions;
 
--- CSharpMath/Display/InkExtensions.cs
+++ CSharpMath/Display/InkExtensions.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Drawing;
+using CSharpMath.Atom.Atoms;
 using CSharpMath.Display.Displays;
 using CSharpMath.Display.FrontEnd;
-using CSharpMath.Atom.Atoms;
 
 namespace CSharpMath {
   using Display;
