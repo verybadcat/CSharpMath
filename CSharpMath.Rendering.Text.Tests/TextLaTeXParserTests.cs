d5c3cc39ad70791ea031ce0b7becf5e1f192fbd1
Hadrian Tang <hadrianwttang@outlook.com>
2026-08-31 01:30:24 +0800

    test: update unicode word-wrap baselines

--- CSharpMath.Rendering.Text.Tests/TextLaTeXParserTests.cs
+++ CSharpMath.Rendering.Text.Tests/TextLaTeXParserTests.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xunit;
 using CSharpMath.Display.Displays;
 using CSharpMath.Rendering.BackEnd;
 using CSharpMath.Rendering.Text;
+using Xunit;
 
 namespace CSharpMath.Rendering.Text.Tests {
   public class TextLaTeXParserTests {
