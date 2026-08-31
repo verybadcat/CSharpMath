726320e2a760b0dce57695e5de4610d948c54316
Happypig375 <hadrianwttang@outlook.com>
2026-08-31 13:28:00 +0800

    test(rendering): cover generic not overlay

--- CSharpMath.Rendering.Tests/TestRenderingMathData.cs
+++ CSharpMath.Rendering.Tests/TestRenderingMathData.cs
@@ -75,6 +75,7 @@ c\alpha + d\gamma & c\beta + d \delta
 
     public const string Nothing = @"";
     public const string NotEquals = @"\not=";
+    public const string NotApproximately = @"\not\approx";
     public const string Overline = @"\overline{Overline}";
     public const string Pi = @"\pi";
     public const string Phi = @"\phi";
